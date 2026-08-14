2bed33916a342849740a74b4d0f717333e95e7f8
LauraArroyo10 <la.arroyo.rivera@gmail.com>
2026-08-13 23:18:13 -0600

    Refactor server routes and improve endpoint handling

--- server.ts
+++ server.ts
@@ -3,26 +3,27 @@ import app from "./src/app";
 import express from "express";
 
 // import created routes
-// import userRoutes from './src/routes/userRoutes';
 import authRoutes from "./src/routes/authRoutes";
 import reviewsRoutes from "./src/routes/reviewsRoutes";
 import businessRoutes from "./src/routes/businessesRoutes";
 import searchRoutes from "./src/routes/searchRoutes";
 import favoritesRoutes from "./src/routes/favoritesRoutes";
 import productsRoutes from "./src/routes/productsRoutes";
-import userRoutes from "./src/routes/userRoutes"; // O la ruta correspondiente de importación
+import userRoutes from "./src/routes/userRoutes";
 
 app.use("/api/auth", authRoutes);
 app.use("/api/businesses", businessRoutes);
 app.use("/api/businesses", reviewsRoutes);
 app.use("/api/search", searchRoutes);
+app.use("/api/users", favoritesRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/products", productsRoutes);
-app.use("/api/uploads", express.static("uploads"));  
+app.use("/api/uploads", express.static("uploads"));
 
+app.use("/api", (_req, res) => {
+	res.status(404).json({ message: "Endpoint not found" });
+});
 
-app.use("api/uploads", express.static("uploads"));
-
-
-app.use("/api", (_req, res) => {res.status(404).json({ message: "Endpoint not found" });});
-app.listen(env.PORT, () => {console.log(`Server running on port ${env.PORT}`);});
+app.listen(env.PORT, () => {
+	console.log(`Server running on port ${env.PORT}`);
+});
